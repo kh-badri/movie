19d0dace33748f1e20d8c9389e42d12c51b42e0d
kh-badri <khbadri12@gmail.com>
2023-11-20 14:11:56 +0700

    update

---
+++ tests/likeMovie.test.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
+
+describe('Liking A Movie', () => {
+  const addLikeButtonContainer = () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+  };
+
+  beforeEach(() => {
+    addLikeButtonContainer();
+  });
+
+  it('should show the like button when the movie has not been liked before', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this movie"]')).toBeTruthy();
+  });
+
+  it('should not show the unlike button when the movie has not been liked before', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unlike this movie"]')).toBeFalsy();
+  });
+
+  it('should be able to like the movie', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+
+    // Tidak ada film yang ganda
+    expect(await FavoriteMovieIdb.getAllMovies()).toEqual([{ id: 1 }]);
+    await FavoriteMovieIdb.deleteMovie(1);
+  });
+});
